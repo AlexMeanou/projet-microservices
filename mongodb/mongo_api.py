a64f5784cf6f20483500621fd2aa4ff5b223dd2e
amelie-ort <ortunoamelie@gmail.com>
2022-03-17 11:25:49 +0100

    mongoapi

--- mongodb/mongo_api.py
+++ mongodb/mongo_api.py
@@ -21,7 +21,10 @@ async def root():
 async def get_one_by_id(movie_id: str):
     return [ a for a in movies.find({"_id" : movie_id})]
 
-@app.get("/movies")
-async def get_movies_order_by_date():
-    return movies.find()
+@app.get("/movies/{page}")
+async def get_movies_by_page(page : int):
+    return [m for m in movies.find().skip(page * 10).limit(10)]
 
+@app.get("/action_movies/{page}")
+async def get_movies_by_page(page : int):
+    return [m for m in movies.find().skip(page * 10).limit(10)]
