259875c91cd76313e5cce9488c33f89865b98d30
Alexandre MEANARD <meanardale@cy-tech.fr>
2022-03-17 12:14:33 +0100

    Api

--- imdb_api/functions.py
+++ imdb_api/functions.py
@@ -1,5 +1,17 @@
+import string
 import requests
 
 
-def testGet():
-    return requests.get('https://imdb-api.com/API/Search/k_xgqcmk1o/leon the professional')
+def get_movie_by_id(id: string):
+    return requests.get('https://imdb-api.com/API/Title/k_xgqcmk1o/{id}/')
+
+# def getMoviesListID(idList : list):
+#     res = []
+#     for id in idList:
+#         res.append(requests.get('https://imdb-api.com/API/Title/k_xgqcmk1o/{id}/'))
+#     return res
+
+def get_movie_by_genre(genre: list):
+    req = "https://imdb-api.com/API/AdvancedSearch/k_xgqcmk1o/?genres=" + genre
+    print (req)
+    return requests.get(req)
--- imdb_api/imdb_api.py
+++ imdb_api/imdb_api.py
@@ -11,7 +11,32 @@ async def root():
 
 
 @app.get('/proposistions')
-async def getRadom():
-    res = testGet()
-    print(res.json())
+async def movie_by_genre(id):
+    res = get_movie_by_id(id=id)
+    # print(res.json())
     return res.json()
+
+
+@app.get('/movie/genre')
+async def movie_by_genre(genre):
+    res = get_movie_by_genre(genre=genre)
+    # print(res.json())
+    return res.json()
+
+
+# @app.get('/listMovies')
+# async def test():
+#     res = getMoviesListID([
+#         "tt0000011",
+#         "tt0000012",
+#         "tt0000013",
+#         "tt0000016",
+#         "tt0000017",
+#         "tt0000018",
+#         "tt0000019",
+#         "tt0000020",
+#         "tt0000015",
+#         "tt0000023"
+#         ]
+#         )
+#     return res.json()
