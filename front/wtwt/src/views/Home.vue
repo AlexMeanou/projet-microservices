88d88aa5e3c1abf499a1784368937ac1c9ff42bc
amelie-ort <ortunoamelie@gmail.com>
2022-03-29 22:14:24 +0200

    wip

--- front/wtwt/src/views/Home.vue
+++ front/wtwt/src/views/Home.vue
@@ -109,16 +109,17 @@ export default{
     },
     changePage(){
       console.log("filerList",this.filterList);
-      // this.axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-      // console.log("page numero",this.page);
-      //  this.axios.get('http://127.0.0.1:8000/movies/' + this.page)
-      // .then(response =>{
-      //   this.movies = response.data
-      //   console.log(this.movies)
-      // } );
+      this.axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+      console.log("page numero",this.page);
+       this.axios.get('http://localhost:8585/movies/Short/' + this.page)
+      .then(response =>{
+        this.movies = response.data
+        console.log(this.movies)
+      } );
     }
   }
 }
+
 </script>
 
 <style scoped lang="scss">
